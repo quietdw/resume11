74550ce7d280dac6b7eb1efd8840ecaf2b5bda19
quietdw <1044569102@qq.com>
2018-12-01 15:41:55 +0800

    增加简单点击滚动

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
-setTimeout(function(){
-  
-    loadingContainer.classList.remove('active')
-  },2000)
+setTimeout(function () {
 
-window.onscroll = function(){
-    (window.scrollY>0)?topNavBar.classList.add('sticky'):topNavBar.classList.remove('sticky')
+    loadingContainer.classList.remove('active')
+}, 0)
+
+window.onscroll = function () {
+    (window.scrollY > 0) ? topNavBar.classList.add('sticky'): topNavBar.classList.remove('sticky')
 }
 
 var topNavBarLi = document.querySelectorAll('.topNavBarInner>nav>ul>li')
-for(let i=0;i<topNavBarLi.length;i++){
-    topNavBarLi[i].onmouseenter=function(e){
-        
-        var oLi=e.target
+for (let i = 0; i < topNavBarLi.length; i++) {
+    topNavBarLi[i].onmouseenter = function (e) {
+        let oLi = e.target
         oLi.classList.add('active')
-        // var brother = topNavBarLi[i].nextSibling
-        // while(topNavBarLi[i].nextSibling!='UL'){
-        //     brother = brother.nextSibling
-        //     console.log(brother)
-        // }
-       
     }
-    topNavBarLi[i].onmouseleave=function(e){
-        
-        var oLi=e.target
+    topNavBarLi[i].onmouseleave = function (e) {
+        let oLi = e.target
         oLi.classList.remove('active')
-        // var brother = topNavBarLi[i].nextSibling
-        // while(topNavBarLi[i].nextSibling!='UL'){
-        //     brother = brother.nextSibling
-        //     console.log(brother)
-        // }
-       
+    }
+}
+
+var topNavBarLiA = document.querySelectorAll('.topNavBarInner>nav>ul>li>a')
+for (let i = 0; i < topNavBarLi.length; i++) {
+    topNavBarLi[i].onclick = function (e) {
+        e.preventDefault()
+        let oA = e.target
+        let aHref = oA.getAttribute('href')
+        let oTarget = document.querySelector(aHref)
+        let scrollH = oTarget.offsetTop
+        console.log(scrollH)
+        window.scrollTo(0,scrollH-75)
+        //window.document.scrollTop=scrollHeight + 'px'
     }
 }
