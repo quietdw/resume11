1380a44680f2dfccae0094473d035b91b4488653
quietdw <1044569102@qq.com>
2019-01-16 23:36:16 +0800

    手机页面更改

--- js/sticky-topbar.js
+++ js/sticky-topbar.js
@@ -4,22 +4,43 @@
 
     let controller = {
         view: null,
+        menuBtn: null,
+        smallSizeNav:null,
+        clickflag:true,
         init: function (view) {
             this.view = view
+            this.menuBtn = this.view.querySelector('.menuBtn')
+            this.smallSizeNav = this.view.querySelector('.smallSize')
             this.bindEvents()
-
+            
         },
         bindEvents: function () {
             (window.scrollY > 0) ? this.active(): this.unactive()
             window.addEventListener('scroll', () =>{
                 (window.scrollY > 0) ? this.active(): this.unactive()
             })
+            this.menuBtn.addEventListener('click',()=>{
+                if(this.clickflag){
+                    this.showSmallSizeNav()
+                    this.clickflag = !this.clickflag
+                }else{
+                    this.hideSmallSizeNav()
+                    this.clickflag = !this.clickflag
+                }
+            })
+            
         },
         active: function () {
             this.view.classList.add('sticky')
         },
         unactive: function () {
             this.view.classList.remove('sticky')
+        },
+        showSmallSizeNav:function(){
+            this.smallSizeNav.classList.add('active')
+        },
+        hideSmallSizeNav:function(){
+            this.smallSizeNav.classList.remove('active')
         }
     }
 
