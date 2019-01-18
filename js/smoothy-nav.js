45dc089c1227d36f3b34c78280fb1f834fdd17e3
quietdw <1044569102@qq.com>
2019-01-18 13:23:57 +0800

    作品集样式

--- js/smoothy-nav.js
+++ js/smoothy-nav.js
@@ -1,33 +1,32 @@
 ! function () {
     let view = View('.topNavBarInner>nav>ul>li>a')
-
     let controller = {
         view: null,
         init: function (view) {
             this.view = view
             this.initAnimate()
-            for (let i = 0; i < 4; i++) {
+            for (let i = 0; i < this.view.length; i++) {
                 this.view[i].onclick = function (e) {
-                    e.preventDefault()
-
-                    let currentHeight = window.scrollY
-                    let targetHeight = document.querySelector(e.target.getAttribute('href')).offsetTop - 125
-
-                    let t = Math.abs((targetHeight - currentHeight) / 500)
-                    t > 1 ? t = 1 : t
-                    var coords = {
-                        y: currentHeight
+                    if(document.querySelector(e.target.getAttribute('href'))){
+                        e.preventDefault()
+                        let currentHeight = window.scrollY
+                        let targetHeight = document.querySelector(e.target.getAttribute('href')).offsetTop - 125
+                        let t = Math.abs((targetHeight - currentHeight) / 500)
+                        t > 1 ? t = 1 : t
+                        var coords = {
+                            y: currentHeight
+                        }
+                        var tween = new TWEEN.Tween(coords)
+                                    .to({
+                                        y: targetHeight
+                                    }, t * 1000)
+                                    .easing(TWEEN.Easing.Quadratic.Out)
+                                    .onUpdate(function () {
+                                        window.scrollTo(0, coords.y)
+                                    })
+                                    .start();
                     }
-                    var tween = new TWEEN.Tween(coords)
-                        .to({
-                            y: targetHeight
-                        }, t * 1000)
-                        .easing(TWEEN.Easing.Quadratic.Out)
-                        .onUpdate(function () {
-                            window.scrollTo(0, coords.y)
-                        })
-                        .start();
-                }
+                } 
             }
         },
         initAnimate: function () {
