59e88d7d81f0a25f499bca94627c05c7cd3a010e
quietdw <1044569102@qq.com>
2019-03-23 22:25:51 +0800

    代码格式化

--- js/message.js
+++ js/message.js
@@ -1,52 +1,52 @@
-!function () {
+! function () {
 
     let model = Model({
-        resouceName:'Message'
+        resouceName: 'Message'
     })
     let view = View('.message')[0]
-    let controller = Controller(
-        {
-            leaveMessage:null,
-            messageList:null,
-            init:function(view,model){
-                this.leaveMessage = view.querySelector('#leaveMessage')
-                this.messageList = view.querySelector('ul')
-                this.messageInit()
-                // this.bindEvents() Controller函数已经调用了
-            },
-            messageInit:function(){
-                this.model.fetch().then((message)=> {
-                    let array = message.map((value) => value.attributes);
-                   
-                    array.forEach((value, key) => {
-                        let oLi = document.createElement('li')
-                        let ospanname = document.createElement('span')
-                        let ospanmessage = document.createElement('span')
-                        ospanname.innerText = value.name + ': '
-                        ospanmessage.innerText = value.message
-    
-                        oLi.appendChild(ospanname)
-                        oLi.appendChild(ospanmessage)
-    
-                        this.messageList.prepend(oLi)
-                       
-                    });
-                })
-            },
-            bindEvents:function(){
-                this.leaveMessage.addEventListener('submit',  (e)=> {
-                    e.preventDefault()
-                    this.savemessage()
-                })
-            },
-            savemessage:function(){
-                leaveMessage =  this.leaveMessage
-                let postName = leaveMessage.querySelector('input[name=name]').value
-                let postMessages = leaveMessage.querySelector('input[name=messages]').value
-                
+    let controller = Controller({
+        leaveMessage: null,
+        messageList: null,
+        init: function (view, model) {
+            this.leaveMessage = view.querySelector('#leaveMessage')
+            this.messageList = view.querySelector('ul')
+            this.messageInit()
+            // this.bindEvents() Controller函数已经调用了
+        },
+        messageInit: function () {
+            this.model.fetch().then((message) => {
+                let array = message.map((value) => value.attributes);
+
+                array.forEach((value, key) => {
+                    let oLi = document.createElement('li')
+                    let ospanname = document.createElement('span')
+                    let ospanmessage = document.createElement('span')
+                    ospanname.innerText = value.name + ': '
+                    ospanmessage.innerText = value.message
+
+                    oLi.appendChild(ospanname)
+                    oLi.appendChild(ospanmessage)
+
+                    this.messageList.prepend(oLi)
+
+                });
+            })
+        },
+        bindEvents: function () {
+            this.leaveMessage.addEventListener('submit', (e) => {
+                e.preventDefault()
+                this.savemessage()
+            })
+        },
+        savemessage: function () {
+            leaveMessage = this.leaveMessage
+            let postName = leaveMessage.querySelector('input[name=name]').value
+            let postMessages = leaveMessage.querySelector('input[name=messages]').value
+
+            if (postName && postMessages) {
                 this.model.save({
-                    name:postName,
-                    message:postMessages
+                    name: postName,
+                    message: postMessages
                 }).then(function (object) {
                     alert('提交成功')
                     let ul = view.querySelector('ul')
@@ -55,16 +55,18 @@
                     let ospanmessage = document.createElement('span')
                     ospanname.innerText = object.attributes.name + ': '
                     ospanmessage.innerText = object.attributes.message
-    
+
                     oLi.appendChild(ospanname)
                     oLi.appendChild(ospanmessage)
-    
-                    ul.appendChild(oLi)
-    
+
+                    ul.prepend(oLi)
+
                 })
+            } else {
+                alert("请写点东西再提交吧~")
             }
         }
-    )
+    })
     // let controller = {
     //     view:null,
     //     leaveMessage:null,
@@ -82,7 +84,7 @@
     //     messageInit:function(){
     //         this.model.fetch().then((message)=> {
     //             let array = message.map((value) => value.attributes);
-               
+
     //             array.forEach((value, key) => {
     //                 let oLi = document.createElement('li')
     //                 let ospanname = document.createElement('span')
@@ -94,7 +96,7 @@
     //                 oLi.appendChild(ospanmessage)
 
     //                 this.messageList.appendChild(oLi)
-                   
+
     //             });
     //         })
     //     },
@@ -108,7 +110,7 @@
     //         leaveMessage =  this.leaveMessage
     //         let postName = leaveMessage.querySelector('input[name=name]').value
     //         let postMessages = leaveMessage.querySelector('input[name=messages]').value
-            
+
     //         this.model.save({
     //             name:postName,
     //             message:postMessages
@@ -129,7 +131,7 @@
     //         })
     //     }
     // }
-    
-    controller.init.call(controller,view,model)
+
+    controller.init.call(controller, view, model)
 
 }.call()
