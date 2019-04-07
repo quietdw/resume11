02e85d418f30734c784355d0b9683c2f89b43615
quietdw <1044569102@qq.com>
2019-04-07 21:25:17 +0800

    新增项目，网页优化

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+ScrollReveal().reveal('.widget', {
+  origin: 'bottom',
+  distance: '100px',
+  duration: 500,
+  easing: 'cubic-bezier(0.5, 0, 0, 1)',
+  delay: 0,
+  opacity: 0,
+  reset: true,
+  scale: 0.8,
+});
+ScrollReveal().reveal('.widget1', {
+  origin: 'bottom',
+  distance: '100px',
+  duration: 500,
+  easing: 'cubic-bezier(0.5, 0, 0, 1)',
+  delay: 300,
+  opacity: 0,
+  reset: true,
+  scale: 0.8,
+});
 window.onload = function () {
   setTimeout(() => {
     loadingContainer.classList.remove('active')
