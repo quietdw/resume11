c59373e8af4c5b7965b4560794104418c9fc1812
quietdw <1044569102@qq.com>
2019-04-11 21:59:36 +0800

    取消加载动画

--- js/main.js
+++ js/main.js
@@ -8,18 +8,3 @@ ScrollReveal().reveal('.widget', {
   reset: true,
   scale: 0.8,
 });
-ScrollReveal().reveal('.widget1', {
-  origin: 'bottom',
-  distance: '100px',
-  duration: 500,
-  easing: 'cubic-bezier(0.5, 0, 0, 1)',
-  delay: 300,
-  opacity: 0,
-  reset: true,
-  scale: 0.8,
-});
-window.onload = function () {
-  setTimeout(() => {
-    loadingContainer.classList.remove('active')
-  }, 300)
-}
